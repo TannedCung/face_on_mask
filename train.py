4a50e8bc3fbae83f6a683b3fa268fd321af01663
TannedCung <tando1903@gmail.com>
2021-06-23 17:22:36 +0700

    negpos ratio to 5, update ckp

--- train.py
+++ train.py
@@ -102,7 +102,7 @@ else:
 cudnn.benchmark = True
 
 optimizer = optim.SGD(net.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
-criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 3, 0.35, False, args)
+criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 5, 0.35, False, args)
 
 priorbox = PriorBox(cfg, image_size=(img_dim, img_dim))
 with torch.no_grad():
